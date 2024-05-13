6b232d5b4fc965ce3af87a654e3d2b4c5448fce8
HamCat <HamCat.GitHub.Cbtis@gmail.com>
2024-05-13 12:17:47 -0600

    version 4 con while y for :c

---
+++ ejercicio43v4.c
@@ -0,0 +1,45 @@
+int main()
+{
+    int contr,list,cont,intentos,arti,precio,total;
+    char am[50];
+        while (contr<=3 || contr==0)
+        {
+            printf("Ingresa Tu Nombre\n");
+            scanf("%s",&am);
+            printf("Ingresa Tu Numero De Lista\n");
+            scanf("%d",&list);
+            printf("Ingresa la contraseña\n");
+            scanf("%d",&contr);
+            if (contr==1234)
+            {
+                printf("BIENVENIDO AL SISTEMA\n");
+                intentos=0;
+            }
+            else
+            {
+               intentos--; 
+            }
+            if (intentos>0)
+            {
+                printf("numero de intentos excedidos te quedan:%d\n",intentos);
+            }
+            else
+            {
+                printf("has exedido tus intentos");
+            }
+             printf("¿Cuantos Articulos Llevas?\n");
+             scanf("%i",&arti);
+             for (i = 1; i < arti; i++)
+            {
+            printf("ingresa su precio\n");
+            scanf("%i",&precio);
+            total += precio;
+            }
+            printf("el total a pagar es de: %d\n",total);
+            if (arti==0)
+            {
+            printf("COMPRA FINALIZADA");
+            }
+            return 0;
+        }
+        
